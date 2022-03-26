0c27d83baf7f85816fff2361852cf1e58208184b
DavidHydroneWang <1290877079@qq.com>
2022-03-27 00:17:23 +0800

    2022-03-26

---
+++ Python/ChangeFileName/changename3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os,re,sys,math
+
+
+class PathHandler:
+    """
+    分别对文件和文件夹进行处理
+    """
+
+    def __init__(self):
+        self.path = os.listdir(os.getcwd())
+        self.fatherpath = os.getcwd()
+
+    def Change(self):
+#        print(self.path)
+        for i in self.path:
+            if os.path.isfile(i):
+                name = i
+                name = re.sub(r'[\*>#？：，：。、【】:\?,\n\t]','',name)
+                name = re.sub(r'[\（]','(',name)
+                name = re.sub(r'[\）]',')',name)
+                temp = name.split('.')
+                newname = ""
+                for l in range(0,len(temp)-1):
+                    newname += temp[l]
+                newname = newname.partition("by")[0]
+                newname += "."
+                newname += temp[-1]
+                os.rename(i,newname)
+            else:
+#                print(i)
+#                print(str(i))
+                os.chdir(str(i))
+                inside_change = PathHandler()
+                inside_change.Change()
+                os.chdir(self.fatherpath)
+
+
+if __name__ == "__main__":
+    chang = PathHandler()
+    chang.Change()
