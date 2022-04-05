4ae5e5a5ea3491ca7ea688d4f07fbe00f9f26c31
DavidHydroneWang <1290877079@qq.com>
2022-04-05 19:41:03 +0800

    2022-04-05

--- Python/THE_BIG_BOOK_OF_SMAL_PYTHON_PROJECTS/Project15-2022-04-03/deepcave.py
+++ Python/THE_BIG_BOOK_OF_SMAL_PYTHON_PROJECTS/Project15-2022-04-03/deepcave.py
@@ -13,4 +13,38 @@ import random, sys, time
 WIDTH = 70  # (!) try changing to 10 or 30
 PAUSE_AMOUT = 0.05  # (!) try changing to 0 or 1.0
 
-print('Deep Cave, by Al Sweigart al@inventwith')
+print('Deep Cave, by Al Sweigart al@inventwithpython.com')
+print('Press Ctrl-C to QUIT.')
+time.sleep(2)
+
+leftWidth = 30
+gapWidth = 10
+
+while True:
+    # display the tunnel segment
+    righthWidth = WIDTH - leftWidth - gapWidth
+    print(('#' * leftWidth) + (' ' * gapWidth) + ('#' * righthWidth))
+    # check for Ctrl-C press during the pause.
+    try:
+        time.sleep(PAUSE_AMOUT)
+    except KeyboardInterrupt:
+        sys.exit()
+
+    # adjust the left side width.
+    diceRoll = random.randint(1, 6)
+    if diceRoll == 1 and leftWidth > 1:
+        leftWidth = leftWidth - 1
+    elif diceRoll == 2 and leftWidth + gapWidth < WIDTH - 1:
+        leftWidth = leftWidth + 1
+    else:
+        pass
+
+    # Adjust the gap width:
+    # (!) Try uncommenting out all of the following code:
+    # diceRoll = random.randint(1, 6)
+    # if diceRoll == 1 and gapWidth > 1:
+    # gapWidth = gapWidth - 1 # Decrease gap width.
+    # elif diceRoll == 2 and leftWidth + gapWidth < WIDTH - 1:
+    # gapWidth = gapWidth + 1 # Increase gap width.
+    # else:
+    # pass # Do nothing; no change in gap width.
